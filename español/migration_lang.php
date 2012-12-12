ccc3bf703c3cffe1d7a2c8f567e5e99ac4632612
Felipe González Alfaro <chivi.figa@gmail.com>
2012-12-12 15:20:21 -0300

    Archivos de lenguaje para CodeIgniter 2.1.3

---
+++ español/migration_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['migration_none_found']			= "No se encontraron misgraciones.";
+$lang['migration_not_found']			= "Esta migración no se ha encontrado.";
+$lang['migration_multiple_version']		= "Hay multiples migraciones con el mismo número de versión: %d.";
+$lang['migration_class_doesnt_exist']	= "La clase de migración\"%s\" no se ha encontrado.";
+$lang['migration_missing_up_method']	= "A la clase de migración \"%s\" le falta el método 'up'.";
+$lang['migration_missing_down_method']	= "A la clase de migración \"%s\" le falta el método 'down'.";
+$lang['migration_invalid_filename']		= "Migración \"%s\" tiene un nombre de archivo no válido..";
+
+
+/* End of file migration_lang.php */
+/* Location: ./system/language/español/migration_lang.php */
